096f668391f1b791e99c88e98d06960c909273c2
Christi Lato <christilato@Christis-MacBook-Air.local>
2022-09-05 11:56:51 -0500

    Cleaned up with tutor. was able to get questions appear. just need to finish with end screen and do the scores java.

--- Assets/Java/script.js
+++ Assets/Java/script.js
@@ -1,44 +1,67 @@
 
-// import {questions} from "./questions";
-var questions = [import {questions} from "./questions"]
+
 var pTags = document.querySelectorAll("p");
 var startBtn = document.getElementById("start");
-var timerDiv = document.querySelector("timer");
-var questionsDiv = document.querySelector ("questions");
-var correctAnswer = localStorage.getItem("correct");
-var incorrectAnswer = localStorage.getItem("incorrect");
+var timerDiv = document.querySelector(".timer");
+var questionsDiv = document.querySelector("#questions");
+var endScreen = document.getElementById("end-screen");
+var questionTitle = document.getElementById("question-title");
+var questionsChoice = document.querySelector('#choices')
 
 pTags[0].setAttribute("style", "font-size: 20px;"); // add attributes to the words in <p>
 
+console.log(questions); // questions are attached 
 
 
 startBtn.addEventListener("click", function() {
     var timer = 60;
-    var timer = setInterval(function()
+    startQuiz()
+    var timerDecrease = setInterval(function()
     {
     timer--;
-    timerDiv.textContent = " ";
+    // String concatenation 
+    // It allows us to combine variables and string for a better user experience
+    timerDiv.textContent = "Time left: " + timer;
+    //Start quiz 
+
     //show final score
      if( timer === 0){
         clearInterval(timer);
+        endScreen.removeAttribute("class")
         // display the scores
     }
 }, 1000);
 })
 
-
-
-for (var index = 0; index < questionsDiv.length; index++) {
-    var response = window.prompt(questions[i].prompt)
-    if (response == questions [i].answer){
-        score++
-        alert ("correct!");
-    } else {
-        alert ("Wrong!");
+function startQuiz() {
+    questionsDiv.removeAttribute("class");
+    // We need to make deconstructed for loop
+    // WE create an index variable 
+    // And we manually go through each questions
+    //Then once the correct answer is given, then we increase our index variable manually
+    //And then pass it into the next aset of questions
+    var i = 0
+    questionTitle.textContent = questions[i].title;
+    for (let j = 0; j < 4; j++) {
+        var button = document.createElement('button')
+        button.textContent = questions[i].choices[j]
+        questionsChoice.append(button)
+        
     }
+ /* We are going to have a condition tocheck if the user clicks the right answer
+ If correct, increase index variable by 1
+ THen re create all the title and choices wiht that new index
+ Else - time from timer
+ */
+
+
+        
+    
 }
 
-alert("you got " + score + "/" + questions.length)
+
+
+// alert("you got " + score + "/" + questions.length)
 
 // does this go in the scores sheet?
 // function showResponse(event){
